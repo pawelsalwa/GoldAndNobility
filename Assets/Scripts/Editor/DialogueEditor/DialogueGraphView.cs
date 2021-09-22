e9d77241aa8c0c43cc046fb101cb486d685529e0
PawelSalwa <siewcasalwus@gmail.com>
2021-09-22 19:23:44 +0200

    separate toolbar class

--- Assets/Scripts/Editor/DialogueEditor/DialogueGraphView.cs
+++ Assets/Scripts/Editor/DialogueEditor/DialogueGraphView.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Dialogue;
-using UnityEditor;
 using UnityEditor.Experimental.GraphView;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,7 +10,6 @@ namespace Editor.DialogueEditor
 	internal class DialogueGraphView : GraphView
 	{
 		private readonly DialogueData data;
-		private List<DialogueData> dialogueAssets;
 		private Port startPort;
 
 		internal DialogueGraphView(DialogueData data)
@@ -53,7 +50,6 @@ namespace Editor.DialogueEditor
 		private void CreateEdge(Connection connection)
 		{
 			var dialogueNodes = nodes.Where(n => n is DialogueNode).Cast<DialogueNode>();
-			// var dialogueNodes = nodes.ToList().Cast<DialogueNode>().Where(n => n != null).ToList();
 			
 			var outputNode = dialogueNodes.FirstOrDefault(IsOutputNode);
 			var inputNode = dialogueNodes.FirstOrDefault(IsInputNode);
@@ -144,48 +140,7 @@ namespace Editor.DialogueEditor
 			grid.StretchToParentSize();
 		}
 
-		private void GenerateToolbar()
-		{
-			var toolbar = new Toolbar();
-			var nodeCreateButton = new Button(OnCreateNodeButton) {text = "CreateNode"};
-			toolbar.Add(nodeCreateButton);
-
-			dialogueAssets = GetDialogueAssets();
-			var assetsLabels = dialogueAssets.Select(x => x.name).ToList();
-			var idx = dialogueAssets.IndexOf(data);
-			var dropdown = new DropdownField("Edited dialogue: ", assetsLabels, idx, GetSelectedDialogueName, GetDialogueNameFromLabelName);
-			dropdown.RegisterValueChangedCallback(DropdownChangedCallback);
-			
-			toolbar.Add(dropdown);
-			Add(toolbar);
-		}
-
-		private void DropdownChangedCallback(ChangeEvent<string> evt)
-		{
-			// Debug.Log($"<color=orange>dropdown change cb {evt.previousValue} to {evt.newValue}</color>");
-			Selection.objects = new Object[] { dialogueAssets.FirstOrDefault(x => x.name == evt.newValue) }; // selection change will rebuild whole graph xP !!
-		} 
-
-		private string GetSelectedDialogueName(string arg)
-		{
-			// Debug.Log($"<color=yellow>GetSelectedDialogueName {arg}</color>");
-			return arg;
-		}
-
-		private string GetDialogueNameFromLabelName(string arg)
-		{
-			// Debug.Log($"<color=yellow>GetDialogueNameFromLabelName {arg}</color>");
-			return arg;
-		}
-
-		private static List<DialogueData> GetDialogueAssets()
-		{
-			var filter = $"t:{typeof(DialogueData)}";
-			var guids = AssetDatabase.FindAssets(filter);
-			var paths = guids.Select(AssetDatabase.GUIDToAssetPath);
-			var dialogues = paths.Select(AssetDatabase.LoadAssetAtPath<DialogueData>).ToList();
-			return dialogues;
-		}
+		private void GenerateToolbar() => Add(new DialogueToolbar(data));
 
 		private void OnCreateNodeButton() => CreateNode(viewport.localBound.center);
 
---
+++ Assets/Scripts/Editor/DialogueEditor/DialogueToolbar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dialogue;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor.DialogueEditor
+{
+	internal class DialogueToolbar : Toolbar
+	{
+		private List<DialogueData> dialogueAssets;
+
+		public DialogueToolbar(DialogueData data)
+		{
+			// var nodeCreateButton = new Button(OnCreateNodeButton) {text = "CreateNode"};
+			// toolbar.Add(nodeCreateButton);
+
+			dialogueAssets = GetDialogueAssets();
+			var assetsLabels = dialogueAssets.Select(x => x.name).ToList();
+			var idx = dialogueAssets.IndexOf(data);
+			var dropdown = new DropdownField("Edited dialogue: ", assetsLabels, idx); //, GetSelectedDialogueName, GetDialogueNameFromLabelName);
+			dropdown.RegisterValueChangedCallback(DropdownChangedCallback);
+			Add(dropdown);
+		}
+		
+		private void DropdownChangedCallback(ChangeEvent<string> evt)
+		{
+			// Debug.Log($"<color=orange>dropdown change cb {evt.previousValue} to {evt.newValue}</color>");
+			Selection.objects = new Object[] { dialogueAssets.FirstOrDefault(x => x.name == evt.newValue) }; // selection change will rebuild whole graph xP !!
+		} 
+		
+		private string GetSelectedDialogueName(string arg)
+		{
+			// Debug.Log($"<color=yellow>GetSelectedDialogueName {arg}</color>");
+			return arg;
+		}
+
+		private string GetDialogueNameFromLabelName(string arg)
+		{
+			// Debug.Log($"<color=yellow>GetDialogueNameFromLabelName {arg}</color>");
+			return arg;
+		}
+
+		private static List<DialogueData> GetDialogueAssets()
+		{
+			var filter = $"t:{typeof(DialogueData)}";
+			var guids = AssetDatabase.FindAssets(filter);
+			var paths = guids.Select(AssetDatabase.GUIDToAssetPath);
+			var dialogues = paths.Select(AssetDatabase.LoadAssetAtPath<DialogueData>).ToList();
+			return dialogues;
+		}
+
+	}
+}
